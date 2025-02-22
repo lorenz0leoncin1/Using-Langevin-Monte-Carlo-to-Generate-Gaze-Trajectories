024d2404e4a37afd0f13a383f4eb9dd2fb25b95a
Lorenzo Leoncini <lorenzo.leoncini@studenti.unimi.it>
2025-02-22 17:47:57 +0100

    Fixing main

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from zs_clip_seg import *
     
 def main():
 
-    # Data path ----------------------------------------------------------
+    # Data path -------------------------------------------------------------------------
     input_dir = "COCOSearch18-images-TP/images/"
     task = "keyboard"
     output_dir = f"output/images/{task}"
     name = "000000006608.jpg"
 
-    # Choose the algorithm -----------------------------------------------
-    algorithm = 'ula' 
+    # Choose the algorithm --------------------------------------------------------------
+    algorithm = 'ula'   # Type of algorithm to use
+    n = 10              # Number of scanpaths to simulate
 
-    # Model Parameter ----------------------------------------------------
+    # Model Parameter -------------------------------------------------------------------
     gamma = 0.1  # This is used only in the MALA-Cauchy
 
-    # Use this to test one of the 3 algorithms on a choosen image
+    # Type of execution
+
+    # Type 1: run a choosen algorithm on a given image ----------------------------------
     #fixations, img = run_algorithm(input_dir, output_dir, task, name, algorithm, gamma)
     
+    # Type 2: Simulate N scanpaths, run a choosen algorithm and recieve all the stats of the performances with MM and SM 
     # Pipeline for evaluating all the images in split1 validation set
-    evaluation_pipeline_N(5, algorithm, gamma)
+    evaluation_pipeline_N(n, algorithm, gamma)
 main()
